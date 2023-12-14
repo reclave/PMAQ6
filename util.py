f448085595230d5e8374b436e213e7d7a6db89fe
Raihan Devano Sinaga <99541167+reclave@users.noreply.github.com>
2023-12-14 10:32:31 +0700

    Update util.py

--- util.py
+++ util.py
@@ -4,19 +4,21 @@ import streamlit as st
 from PIL import ImageOps, Image
 import numpy as np
 
+def set_background(image_file):
+    with open(image_file, "rb") as f:
+        img_data = f.read()
+    b64_encoded = base64.b64encode(img_data).decode()
+    style = f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{b64_encoded});
+            background-size: cover;
+        }}
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
+
 def classify(image, model, class_names):
-    """
-    This function takes an image, a model, and a list of class names and returns the predicted class and confidence
-    score of the image.
-
-    Parameters:
-        image (PIL.Image.Image): An image to be classified.
-        model (tensorflow.keras.Model): A trained machine learning model for image classification.
-        class_names (list): A list of class names corresponding to the classes that the model can predict.
-
-    Returns:
-        A tuple of the predicted class name and the confidence score for that prediction.
-    """
     # convert image to (224, 224)
     image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
 
